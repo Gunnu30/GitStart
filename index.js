2772cf3571abf21ea315679ecb80bd3bc57ab9ad
Ashrith <pushpakashrith30@gmail.com>
2025-11-02 05:28:57 +0000

    Second Commit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const age = 22;
 const name = "Ashrith";
 console.log("My name is " + name + " and I am " + age + " years old.");
+
+function isStudent(age){
+    console.log(age < 25 ? name + " is a Student" : name + " is not a Student");
+}
+function canVote(age){
+    console.log(age >= 18 ? name + " can Vote" : name + " cannot Vote");
+}
